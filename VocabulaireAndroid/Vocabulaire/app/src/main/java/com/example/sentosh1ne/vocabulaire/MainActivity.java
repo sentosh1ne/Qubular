4a253f7c8eaaad8a7de4b036041bbf33b1177809
Sentosh1ne <fortrust2@gmail.com>
2016-05-14 13:56:10 +0300

    Imporeted qubu lib

--- VocabulaireAndroid/Vocabulaire/app/src/main/java/com/example/sentosh1ne/vocabulaire/MainActivity.java
+++ VocabulaireAndroid/Vocabulaire/app/src/main/java/com/example/sentosh1ne/vocabulaire/MainActivity.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import General.Entry;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -26,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        Entry entry = null;
     }
 
     @Override
--- VocabulaireAndroid/Vocabulaire/app/src/main/java/com/example/sentosh1ne/vocabulaire/Word.java
+++
@@ -1,9 +0,0 @@
-package com.example.sentosh1ne.vocabulaire;
-
-/**
- * Created by sentosh1ne on 22.03.2016.
- */
-public class Word {
-
-    //placeholder for word class
-}
